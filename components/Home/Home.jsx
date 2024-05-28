a461079cb3ab3e0b5f484ccb5c1d7bdb436042d7
Md.Arifur Rahman sajid <159644399+arifurrahmansajid@users.noreply.github.com>
2024-05-28 09:44:41 +0600

    Add files via upload

---
+++ components/Home/Home.jsx
@@ -0,0 +1,13 @@
+
+const Home = () => {
+    return (
+        <div className="relative flex flex-col justify-center h-screen overflow-hidden">
+    <div className="w-full p-6 m-auto bg-white rounded-md shadow-md lg:max-w-lg">
+        <h1 className="text-3xl font-semibold text-center text-purple-700">Welcome Everyone</h1>
+        
+    </div>
+</div>
+    );
+};
+
+export default Home;
